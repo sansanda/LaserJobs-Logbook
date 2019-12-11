c2e7461ad30815f35d021585cb67a77b5d6eb18c
sansanda <dsanchezsanc@uoc.edu>
2019-12-11 20:17:28 +0100

    Working on Window Update Excel File with a new job data 2 Next:
    Implements design pattern observer for jobsData and jobsDataView

--- Logic/LaserJobs_Book.py
+++ Logic/LaserJobs_Book.py
@@ -7,31 +7,11 @@ David SAnchez Sanchez
 
 """
 
-import xlrd
-
 class LaserJobs_Book(list):
 
 
-    def __init__(self, sourceURL):
-        self.sourceURL = sourceURL
-        self.loadJobsFromExcel(sourceURL)
-
-    def loadJobsFromExcel(self, sourceURL):
-
-        workbook = xlrd.open_workbook(sourceURL, on_demand=True)
-        worksheet = workbook.sheet_by_index(0)
-        columns_row = []  # The row where we stock the name of the column
-
-        for col in range(worksheet.ncols):
-            columns_row.append(worksheet.cell_value(0, col))
-
-        # tronsform the workbook to a list of dictionnary
-        for row in range(1, worksheet.nrows):
-            elm = {}
-            for col in range(worksheet.ncols - 1):
-                elm[columns_row[col+1]] = str(worksheet.cell_value(row, col+1))
-            self.append(elm)
-
+    def __init__(self):
+        list.__init__(self)
 
     def updateJobsSource(self):
         pass
@@ -43,7 +23,6 @@ class LaserJobs_Book(list):
     def createNewJob(self, newjobData):
         newjobData['jobId'] = len(self)
         self.append(newjobData)
-        print(newjobData)
 
     #return jobData as dictionary
     def getJob(self, jobId):
--- Logic/LogicController.py
+++ Logic/LogicController.py
@@ -6,6 +6,7 @@ David SAnchez Sanchez
 
 
 """
+import xlrd
 
 from Logic.LaserJobs_Book import LaserJobs_Book
 
@@ -13,7 +14,7 @@ class LogicController():
 
     def __init__(self,sourceURL):
         self.guiController = None
-        self.laserJobsBook = LaserJobs_Book(sourceURL)
+        self.laserJobsBook = LaserJobs_Book()
 
     def setGuiController(self,guicontroller):
         self.guiController = guicontroller
@@ -22,7 +23,25 @@ class LogicController():
     def getGuiController(self):
         return self.guiController
 
+
+    def loadJobsFromExcel(self, sourceURL):
+
+        workbook = xlrd.open_workbook(sourceURL, on_demand=True)
+        worksheet = workbook.sheet_by_index(0)
+        columns_row = []  # The row where we stock the name of the column
+
+        for col in range(worksheet.ncols):
+            columns_row.append(worksheet.cell_value(0, col))
+
+        # tronsform the workbook to a list of dictionnary
+        for row in range(1, worksheet.nrows):
+            elm = {}
+            for col in range(worksheet.ncols - 1):
+                elm[columns_row[col+1]] = str(worksheet.cell_value(row, col+1))
+            self.laserJobsBook.append(elm)
+
     def createNewJob(self,newJobdata):
         self.laserJobsBook.createNewJob(newJobdata)
+
     def start(self):
         self.guiController.start()
