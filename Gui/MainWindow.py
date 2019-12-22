0409434f7e8bc230737bcb6f39db60dc9b0c50f1
David <dsanchezsanc@uoc.edu>
2019-12-22 21:11:50 +0100

    Implemented:
    basic sorting
    adapt size of main window to computer screen size
    
    v1.0

--- Gui/MainWindow.py
+++ Gui/MainWindow.py
@@ -11,31 +11,35 @@ from tkinter import *
 from tkinter import ttk, messagebox
 from Logic.LaserJobs_Book import LaserJobs_Book
 
+
 class MainWindow():
 
+    def __init__(self, w_scale, h_scale):
 
-    def __init__(self, w, h):
-
-        self.width = w
-        self.height = h
-        self.state = NORMAL
-
-        self.root = tkinter.Tk(className='MainWindow') # we need this for identifying the
+        self.root = tkinter.Tk(className='MainWindow')  # we need this for identifying the
         self.root.protocol("WM_DELETE_WINDOW", self.close)
         self.root.title('Laser-Jobs Manager. Main Window.')
         self.root.iconbitmap('../Gui/icons/laserJobsManager_Icon5.ico')
 
+        self.width = int(self.root.winfo_screenwidth() * w_scale)
+        self.height = int(self.root.winfo_screenheight() * h_scale)
+
+
         # Gets both half the screen width/height and window width/height
-        positionRight = int(self.root.winfo_screenwidth() / 2 - self.width / 2)
-        positionDown = int(self.root.winfo_screenheight() / 2 - self.height / 2)
-        self.root.geometry(str(self.width) + 'x' + str(self.height) + "+" +str(positionRight) + "+" + str(positionDown))
+        positionRight = int(self.width*(1-w_scale))
+        positionDown = int(self.height*(1-h_scale))
+        self.root.geometry(
+            str(self.width) + 'x' + str(self.height) + "+" + str(positionRight) + "+" + str(positionDown)
+        )
 
-        self.root.resizable(0,0)
+        self.state = NORMAL
 
-    def setGuiController(self,guiController):
+        # self.root.resizable(0,0)
+
+    def setGuiController(self, guiController):
         self.guiController = guiController
 
-    def populate(self,jobsTableHeaders):
+    def populate(self, jobsTableHeaders):
         self.create_menu_bar()
         self.create_filter_bar()
         self.create_jobsTable_frame(jobsTableHeaders)
@@ -46,7 +50,7 @@ class MainWindow():
     def create_filter_bar(self):
 
         self.filter_bar_frame = Frame(self.root)
-        self.filter_bar_frame.grid(row=0, column=0, columnspan=6,sticky=W+E+N+S)
+        self.filter_bar_frame.grid(row=0, column=0, columnspan=6, sticky=W + E + N + S)
         Label(self.filter_bar_frame, text='Filter:', anchor=W, width=20, padx=10, pady=10).grid(row=0, column=0)
         # Username entry
         self.filterText = StringVar(self.filter_bar_frame)
@@ -56,12 +60,11 @@ class MainWindow():
         self.filter_entry.config(width=30)
         self.filter_entry.grid(row=0, column=1, sticky=W)
 
-
     def create_contextual_menu(self):
-        #TODO: Create contextual menu. Edit and delete job
+        # TODO: Create contextual menu. Edit and delete job
         self.popup_menu = Menu(self.root, tearoff=0)
-        self.popup_menu.add_command(label="Delete job",command=self.deleteJob)
-        self.popup_menu.add_command(label="Edit job...",command=self.editJob)
+        self.popup_menu.add_command(label="Delete job", command=self.deleteJob)
+        self.popup_menu.add_command(label="Edit job...", command=self.editJob)
 
         self.root.bind("<Button-3>", self.showPopupMenu)  # Button-2 on Aqua
 
@@ -71,14 +74,14 @@ class MainWindow():
         finally:
             self.popup_menu.grab_release()
 
-
     def create_tool_bar(self):
 
         self.tool_bar_frame = Frame(self.root)
-        self.tool_bar_frame.grid(row=2, column=0, columnspan=6,sticky=W+E+N+S)
+        self.tool_bar_frame.grid(row=2, column=0, columnspan=6, sticky=W + E + N + S)
         addRegisterIcon = PhotoImage(file='../Gui/icons/addRegisterIcon_30x30.gif')
         deleteRegisterIcon = PhotoImage(file='../Gui/icons/deleteRegisterIcon_30x30.gif')
-        self.addNewJobButton = Button(self.tool_bar_frame, image=addRegisterIcon, command= self.guiController.showNewJobWindow)
+        self.addNewJobButton = Button(self.tool_bar_frame, image=addRegisterIcon,
+                                      command=self.guiController.showNewJobWindow)
         self.addNewJobButton.image = addRegisterIcon
         self.addNewJobButton.grid(row=1, column=0, padx=5, pady=2)
         self.deleteJobButton = Button(self.tool_bar_frame, image=deleteRegisterIcon, command=self.deleteJob)
@@ -86,7 +89,7 @@ class MainWindow():
         self.deleteJobButton.grid(row=1, column=1, padx=5, pady=2)
 
     def create_Command_Shortcuts(self):
-        #create command shortcuts
+        # create command shortcuts
         self.root.bind('<Delete>', lambda e: self.deleteJob())
         self.root.bind('<Insert>', lambda e: self.guiController.showNewJobWindow())
 
@@ -107,54 +110,55 @@ class MainWindow():
 
     def create_jobsTable_frame(self, jobsTableHeaders):
 
-        rowHeight = 20 #pixels
+        rowHeight = 20  # pixels
         nRows = 25
         style = ttk.Style(self.root)
 
-        style.configure('Treeview', rowheight=rowHeight)                #rowheight in pixels
-        self.jobsTableTree = ttk.Treeview(self.root, height=nRows)      #height in rows
-        self.jobsTableTree.configure(selectmode="browse")               #configure the tree view for only select one row at time
+        style.configure('Treeview', rowheight=rowHeight)  # rowheight in pixels
+        self.jobsTableTree = ttk.Treeview(self.root, height=nRows)  # height in rows
+        self.jobsTableTree.configure(selectmode="browse")  # configure the tree view for only select one row at time
         self.jobsTableTree.grid(row=1, column=0, columnspan=6, sticky=W + E + N + S)
 
         jobsTableHeaders_Order = tuple(x[0] for x in jobsTableHeaders)
         jobsTableHeaders_Text = tuple(x[1] for x in jobsTableHeaders)
         jobsTableHeaders_WidthInPercent = tuple(x[2] for x in jobsTableHeaders)
 
-        self.jobsTableTree["columns"] = jobsTableHeaders_Order #creamos las columnas
+        self.jobsTableTree["columns"] = jobsTableHeaders_Order  # creamos las columnas
 
-        #Configuramos el texto del encabezado de cada columna
+        # Configuramos el texto del encabezado de cada columna
         for order, text in zip(jobsTableHeaders_Order, jobsTableHeaders_Text):
             self.jobsTableTree.heading(order, text=text, anchor=W)
 
-        #Configuramos la anchura de las columnas
-        for order, w in zip(jobsTableHeaders_Order,jobsTableHeaders_WidthInPercent):
-            self.jobsTableTree.column(order, width=int((w/100.0) * self.width), minwidth=int((w/100.0) * self.width), stretch=True)
+        # Configuramos la anchura de las columnas
+        for order, w in zip(jobsTableHeaders_Order, jobsTableHeaders_WidthInPercent):
+            self.jobsTableTree.column(order, width=int((w / 100.0) * self.width),
+                                      minwidth=int((w / 100.0) * self.width), stretch=True)
 
-        #Adding scroll bars
+        # Adding scroll bars
         ysb = Scrollbar(self.root, orient=VERTICAL, command=self.jobsTableTree.yview)
         ysb.grid(row=0, column=0, sticky='ns')
-        ysb.place(x=self.width-20,y=20, height=nRows*rowHeight, width=20) #number of rows x rowheight
+        ysb.place(x=self.width - 20, y=20, height=nRows * rowHeight, width=20)  # number of rows x rowheight
         self.jobsTableTree.configure(yscroll=ysb.set)
 
-        xsb = Scrollbar(self.root, orient=HORIZONTAL, command=self.jobsTableTree.xview)
-        xsb.grid(row=0, column=0, sticky='we')
-        xsb.place(x=1, y=nRows*rowHeight, height=20, width=self.width-20)  # number of rows x rowheight
-        self.jobsTableTree.configure(xscroll=xsb.set)
+        self.detached_children = {}
 
     # jobs is a list of dictionaries
-    # each job is a dictionary
+    # each jobData is a dictionary
 
     def loadJobsData(self, jobs):
         for jobData in jobs:
             jobDataAsList = LaserJobs_Book.getJobDataAsList(jobData)
-            self.jobsTableTree.insert("", 'end', text= str(jobDataAsList[0]), values=jobDataAsList[1:])
+            self.jobsTableTree.insert("", 'end', text=str(jobDataAsList[0]), values=jobDataAsList[1:])
+        self.detached_children = {}
+        self.filterTreeView(self.filterText)
 
     def deleteJob(self):
         selectedJob = self.jobsTableTree.selection()
-        if len(selectedJob)==0:
-            messagebox.showerror("No job have been selected!!!!!", "Please, first select the job you want to delete!!!!!")
+        if len(selectedJob) == 0:
+            messagebox.showerror("No job have been selected!!!!!",
+                                 "Please, first select the job you want to delete!!!!!")
         else:
-            selectedJob_jobId = self.jobsTableTree.item(selectedJob,'text')
+            selectedJob_jobId = self.jobsTableTree.item(selectedJob, 'text')
             self.guiController.deleteJob(selectedJob_jobId)
 
     def editJob(self):
@@ -178,21 +182,35 @@ class MainWindow():
         self.root.mainloop()
 
     def filterTreeView(self, filterText):
-        #TODO: implements filtering of the tree view (at level of LaserJobs_Book)
+        # TODO: implements filtering of the tree view (at level of LaserJobs_Book)
         # The idea is to use two data structures one for the filter matching jobs and the other for the not filter matching jobs
 
-        print(filterText.get())
-        for r in self.jobsTableTree.get_children():
-            print(r)
+        #reatach
+        for child_id in list(self.detached_children.keys()): #avoid the dictionary changed size during iteration error
+            for value in self.detached_children[child_id]:
+                if filterText.get() in str(value):
+                    print(child_id)
+                    self.jobsTableTree.reattach(child_id,'',int(child_id[1:])-1) #reattach in the same original position
+                    del self.detached_children[child_id]
+                    break
 
-    #part of the Observer design pattern implementation
-    #The object
+        #detach
+        for child_id in self.jobsTableTree.get_children():
+            matched = False
+            child_values = self.jobsTableTree.item(child_id)['values']
+            for value in child_values:
+                if filterText.get() in str(value):
+                    matched = True
+                    break
+            if not matched:
+                self.detached_children[child_id] = child_values
+                self.jobsTableTree.detach(child_id)
+
+
+    # part of the Observer design pattern implementation
+    # The object
     def notify(self, values):
-        #first clear the treeview
+        # first clear the treeview
         self.jobsTableTree.delete(*self.jobsTableTree.get_children())
-        #after reload the data
+        # after reload the data
         self.loadJobsData(values)
-
-
-
-
--- main/LaserJobs_Manager.py
+++ main/LaserJobs_Manager.py
@@ -1,6 +1,7 @@
 from Gui.GuiController import GuiController
 from Logic.LogicController import LogicController
 from Gui.MainWindow import MainWindow
+from tkinter import Tk
 
 def main():
     laserJobsPath = '..\\Data\\'
@@ -13,17 +14,17 @@ def main():
     # 10 es el procentaje de ancho de la columna (del total de MainWindow)
     headers = (             ('#0', 'jobID', 5),
                             ('#1', 'Username', 10),
-                            ('#2', 'Date', 10),
-                            ('#3', 'Material', 10),
-                            ('#4', 'Cut_Raster', 10),
-                            ('#5', 'Speed', 10),
+                            ('#2', 'Date', 5),
+                            ('#3', 'Material', 5),
+                            ('#4', 'Cut_Raster', 5),
+                            ('#5', 'Speed', 5),
                             ('#6', 'Power', 5),
                             ('#7', 'DPI', 5),
                             ('#8', 'Freq', 5),
                             ('#9', '#Passes', 5),
-                            ('#10', 'RasterDepth', 10),
-                            ('#11', 'Others', 25))
-    mw = MainWindow(1400, 650)
+                            ('#10', 'RasterDepth', 5),
+                            ('#11', 'Others', 50))
+    mw = MainWindow(0.8, 0.7)
     mw.setGuiController(gc)
     mw.populate(headers)
     gc.setActualWindow(mw)
